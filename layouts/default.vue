78ed96ceed3adc478f5833d49be70d3ee44e4171
vaneath <vaneathzz@gmail.com>
2023-09-01 20:45:20 +0700

    close #2 Adding default layout and index page

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <title>Durian Flower</title>
+    </Head>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <h1> Page index</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
